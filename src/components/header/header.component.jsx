16cfdf12e3d95e7007f3bdd5fa245afc6502b110
saba756 <sabaofficial12@gmail.com>
2021-03-22 10:55:31 +0500

    added cart-icon

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import {Link} from 'react-router-dom'
 import { auth } from '../../firebase/firebase.utils'
 import {ReactComponent as Logo} from '../../assests/crown.svg';
+import CartIcon from '../cart-icon/cart-icon.component'
+import { connect } from 'react-redux'
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import './header.styles.scss';
-const Header =({currentUser}) =>(
+const Header =({currentUser , hidden}) =>(
     <div className="header"> 
         <Link  className="logo-container" to="/">
         <Logo className="logo"/>
@@ -16,12 +19,23 @@ const Header =({currentUser}) =>(
              CONTACT
             </Link>
             {
-                currentUser ?
-                <div className="option" onClick={()=>auth.signOut()}>SIGN OUT</div> 
+                currentUser ? (
+                <div className="option" onClick={()=>auth.signOut()}>SIGN OUT</div> )
                 :
-                <Link className="option" to="/signin"></Link>
+                (<Link className="option" to="/signin">  Sign In</Link>)
             }
+            <CartIcon/>
     </div>
+    {
+        hidden ? null :
+             <CartDropdown/>
+    }
+   
     </div>
-)
-export default Header;
+  
+);
+const mapStateToProps = ({user: {currentUser} , cart :{hidden}}) =>({
+currentUser,
+hidden
+});
+export default connect(mapStateToProps)(Header);
---
+++ src/redux/user/user-reducer.js
@@ -0,0 +1,18 @@
+import { UserActionTypes} from './usertypes';
+const INTIAL_STATE={
+    currentUser: null
+}
+
+const userReducer = (state = INTIAL_STATE, action) =>{
+switch(action.type){
+    case UserActionTypes.SET_CURRENT_USER:
+        return{
+            ...state,
+            currentUser: action.payload
+        }
+    default:
+        return state;
+}
+};
+
+export default userReducer;
